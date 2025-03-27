87fb495a59cc68be3ee801d489a5ca30138eb281
Minh Vương Quang <vuongquangminh120802@gmail.com>
2025-03-27 17:11:21 +0700

    refactor

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,9 @@
 // src/store/index.js
 import { configureStore } from "@reduxjs/toolkit";
-import featureAReducer from "./feature/featureASlice";
 import featureBReducer from "./feature/featureCartSlice";
 
 const store = configureStore({
   reducer: {
-    featureA: featureAReducer,
     featureCart: featureBReducer,
   },
 });
